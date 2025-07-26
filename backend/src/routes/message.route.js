287d1ebd535dba4103f16b37064a3fa9c16a3522
SyedNaveedulhasan <syednaveedulhasan123@gmail.com>
2025-07-26 23:48:06 +0500

    changes in routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,8 +6,7 @@ import { getMessage } from "../controllers/message.controller.js";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessage);
-
+router.get("/chat/:id", protectRoute, getMessage);
 router.post("/send/:id", protectRoute, sendMessage);
 
 
